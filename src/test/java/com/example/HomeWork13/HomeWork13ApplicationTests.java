727de94577e5fa7eb27c8334eed8e774a63e2502
Alex <alexzviadaze7@gmail.com>
2025-07-01 22:40:51 +0400

    Initial commit

---
+++ src/test/java/com/example/HomeWork13/HomeWork13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HomeWork13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
